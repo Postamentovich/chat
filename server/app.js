0e08436687b4b1a64471e554b000e6cec5dcb4d7
postamentovich <postamentovich@gmail.com>
2020-02-16 00:06:37 +0300

    add rooms

--- server/app.js
+++ server/app.js
@@ -2,8 +2,24 @@ const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
+const m = (name, text, id) => ({ name, text, id })
+
 io.on('connection', (socket) => {
-  console.log('connected')
+  socket.on('userJoined', (data, cb) => {
+    if (!data.name || !data.room) {
+      return cb('Incorrect data')
+    }
+
+    socket.join(data.room)
+
+    cb({ userId: socket.id })
+
+    socket.emit('newMessage', m('admin', `Welcome ${data.name}`))
+
+    socket.broadcast
+      .to(data.room)
+      .emit('newMessage', m('admin', `User ${data.name} joined to room`))
+  })
 
   socket.on('createMessage', (data) => {
     setTimeout(() => {
