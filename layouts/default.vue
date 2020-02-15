4654a1ffa8a42c270bf45f9df89316b2248f7f98
postamentovich <postamentovich@gmail.com>
2020-02-15 23:49:15 +0300

    add chat page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,62 @@
 <template>
+  <!-- App.vue -->
+
   <v-app app>
-    <nuxt />
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list subheader>
+        <v-subheader>Users list</v-subheader>
+
+        <v-list-item v-for="user in users" :key="user.id" @click.prevent>
+          <v-list-item-content>
+            <v-list-item-title v-text="user.name"></v-list-item-title>
+          </v-list-item-content>
+
+          <v-list-item-icon>
+            <v-icon :color="user.id === 2 ? 'primary' : 'grey'"
+              >mdi-chat</v-icon
+            >
+          </v-list-item-icon>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar color="deep-purple accent-4" app dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-btn @click="exit" icon>
+        <v-icon>mdi-arrow-left</v-icon>
+      </v-btn>
+      <v-toolbar-title>Chat Room {{ user.room }}</v-toolbar-title>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-content>
+      <!-- Provides the application the proper gutter -->
+      <!-- If using vue-router -->
+      <div>
+        <nuxt />
+      </div>
+    </v-content>
   </v-app>
 </template>
 
 <script>
-export default {}
+import { mapState, mapMutations } from 'vuex'
+
+export default {
+  data: () => ({
+    drawer: true,
+    users: [
+      { id: 1, name: 'User 1' },
+      { id: 2, name: 'User 2' }
+    ]
+  }),
+  computed: mapState(['user']),
+  methods: {
+    ...mapMutations(['clearData']),
+    exit() {
+      this.$router.push('/?message=leftChat')
+      this.clearData()
+    }
+  }
+}
 </script>
--- pages/chat.vue
+++ pages/chat.vue
@@ -1,5 +1,11 @@
 <template>
-  <h1>{{ user.name }}</h1>
+  <div>
+    <ul>
+      <li v-for="m in messages" :key="m.text">
+        {{ m.text }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -12,6 +18,6 @@ export default {
       title: `Chat - ${this.user.room}`
     }
   },
-  computed: mapState(['user'])
+  computed: mapState(['user', 'messages'])
 }
 </script>
